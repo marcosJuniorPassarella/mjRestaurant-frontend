da0f1a686543d7a5c59e30060d714c225aaee2e7
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2022-03-12 13:59:50 -0300

    main - Added routes system

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import { Button } from "../components/UI/Button"
 import Link from "next/link"
 import { AuthContext } from '../contexts/AuthContext';
 import { toast } from "react-toastify"
+import { canSSRGuest } from '../utils/canSSRGuest';
 
 export default function Home() {
   const { signIn } = useContext(AuthContext)
@@ -65,3 +66,9 @@ export default function Home() {
     </>
   )
 }
+
+export const getServerSideProps = canSSRGuest(async (ctx) => {
+  return {
+    props: {}
+  }
+})
---
+++ src/utils/canSSRGuest.ts
@@ -0,0 +1,27 @@
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+} from "next";
+import { parseCookies } from "nookies";
+
+// FUNÇÃO PARA PÁGINAS QUE SÓ PODEM SER ACESSADAS POR VISITANTES
+export function canSSRGuest<P>(fn: GetServerSideProps<P>) {
+  return async (
+    ctx: GetServerSidePropsContext
+  ): Promise<GetServerSidePropsResult<P>> => {
+    const cookies = parseCookies(ctx);
+
+    // SE O USUÁRIO ACESSAR A PÁGINA POREM JÁ ESTANDO LOGADO REDIRECIONAMOS ELE
+    if (cookies["@nextauth.token"]) {
+      return {
+        redirect: {
+          destination: "/dashboard",
+          permanent: false,
+        },
+      };
+    }
+
+    return await fn(ctx);
+  };
+}
