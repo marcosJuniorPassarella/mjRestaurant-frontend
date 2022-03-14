4aeb07aa7e7b6d0cd804311a49cd64746bf03591
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2022-03-14 18:53:26 -0300

    main - Added Modal and get and close orders

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,10 +1,13 @@
+import { useState } from 'react';
 import Head from "next/head"
 import { canSSRAuth } from "../../utils/canSSRAuth"
 import { Header } from "../../components/Header"
 import styles from './styles.module.scss'
 import { FiRefreshCcw } from 'react-icons/fi'
 import { setupAPIClient } from '../../services/api';
-import { useState } from 'react';
+import Modal from 'react-modal';
+import { ModalOrder } from '../../components/ModalOrder';
+import { toast } from 'react-toastify';
 
 type OrderProps = {
     id: string;
@@ -17,12 +20,64 @@ interface HomeProps {
     orders: OrderProps[];
 }
 
+export type OrderItemProps = {
+    id: string;
+    amount: number;
+    order_id: string;
+    product_id: string;
+    product: {
+        id: string;
+        name: string;
+        desription: string;
+        price: string;
+        banner: string;
+    }
+    order: {
+        id: string;
+        table: string | number;
+        status: boolean;
+        name: string | null;
+    }
+}
+
 export default function Dashboard({ orders }: HomeProps) {
     const [orderList, setOrderList] = useState(orders || [])
+    const [modalItem, setModalItem] = useState<OrderItemProps[]>();
+    const [modalVisible, setModalVisible] = useState(false)
 
-    function handleModalView(id: string){
-        alert(id)
+    function handleCloseModal() {
+        setModalVisible(false)
     }
+
+    async function handleOpenModalView(id: string) {
+        const apiClient = setupAPIClient();
+        const response = await apiClient.get('/orders/details', {
+            params: {
+                order_id: id
+            }
+        })
+        setModalItem(response.data);
+        setModalVisible(true)
+    }
+
+    async function handleFinishItem(order_id: string) {
+        const apiClient = setupAPIClient();
+        await apiClient.put('/order/finish', { order_id })
+
+        const response = await apiClient.get('/orders');
+        setOrderList(response.data);
+        setModalVisible(false);
+        toast.success('Pedido finalizado com sucesso!')
+    }
+
+    async function handleRefreshOrders() {
+        const apiClient = setupAPIClient();
+        const response = await apiClient.get('/orders');
+        setOrderList(response.data)
+        toast.success('Pedidos atualizados com sucesso!')
+    }
+
+    Modal.setAppElement('#__next');
 
     return (
         <>
@@ -34,15 +89,19 @@ export default function Dashboard({ orders }: HomeProps) {
 
                     <div className={styles.containerHeader}>
                         <h1>Últimos pedidos</h1>
-                        <button>
+                        <button onClick={handleRefreshOrders}>
                             <FiRefreshCcw size={25} color="#3fffa3" />
                         </button>
                     </div>
                     <article className={styles.listOrders}>
 
+                        {orderList.length === 0 && (
+                            <span className={styles.noneOrders}>Nenhum pedido foi encontrado...</span>
+                        )}
+
                         {orderList.map(item => (
                             <section key={item.id} className={styles.orderItems}>
-                                <button onClick={() => handleModalView(item.id)}>
+                                <button onClick={() => handleOpenModalView(item.id)}>
                                     <div className={styles.tag}></div>
                                     <span>Mesa {item.table}</span>
                                 </button>
@@ -52,7 +111,14 @@ export default function Dashboard({ orders }: HomeProps) {
 
                     </article>
                 </main>
-
+                {modalVisible && (
+                    <ModalOrder
+                        isOpen={modalVisible}
+                        onRequestClose={handleCloseModal}
+                        order={modalItem}
+                        handleFinishOrder={handleFinishItem}
+                    />
+                )}
             </div>
         </>
     )
